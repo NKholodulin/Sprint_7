5d0332b85a612aa1078ba82928776736f276afb2
holod <holodulinn@mail.ru>
2025-07-14 00:56:04 +0400

    Добавлен тест для оформления заказа

---
+++ src/test/java/CreateOrderTest.java
@@ -0,0 +1,39 @@
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import java.io.File;
+import java.util.Map;
+
+
+public class CreateOrderTest {
+    @BeforeEach
+    public void setUp() {
+
+        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru";
+    }
+
+    @ParameterizedTest
+    @MethodSource("OrderData#orderDataProvider")
+    public void createOrderWithParameters(Map<String, Object> orderData) {
+        createCourier(orderData)
+                .then()
+                .statusCode(201)
+                .body("track", notNullValue());
+    }
+
+    @Step("Send POST request to /api/v1/orders")
+    public static Response createCourier(Map<String, Object> orderData) {
+        Response response = given()
+                .contentType("application/json")
+                .body(orderData)
+                .when()
+                .post("/api/v1/orders");
+        return response;
+    }
+}
