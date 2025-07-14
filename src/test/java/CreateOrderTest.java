6e23c4eac01705cca3ee4cfc5a524969c6ab7979
holod <holodulinn@mail.ru>
2025-07-15 00:44:09 +0400

    Исправлен шаг создания заказа

--- src/test/java/CreateOrderTest.java
+++ src/test/java/CreateOrderTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-import java.io.File;
 import java.util.Map;
 
 
@@ -21,14 +20,14 @@ public class CreateOrderTest {
     @ParameterizedTest
     @MethodSource("OrderData#orderDataProvider")
     public void createOrderWithParameters(Map<String, Object> orderData) {
-        createCourier(orderData)
+        createOrder(orderData)
                 .then()
                 .statusCode(201)
                 .body("track", notNullValue());
     }
 
     @Step("Send POST request to /api/v1/orders")
-    public static Response createCourier(Map<String, Object> orderData) {
+    public static Response createOrder(Map<String, Object> orderData) {
         Response response = given()
                 .contentType("application/json")
                 .body(orderData)
