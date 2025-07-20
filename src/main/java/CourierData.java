88ce5b1e4a7f55ea7e95dcf672e8315df0575a23
holod <holodulinn@mail.ru>
2025-07-20 12:26:49 +0400

    Добавлен плагин Lombok и изменен класс CourierData

--- src/main/java/CourierData.java
+++ src/main/java/CourierData.java
@@ -1,14 +1,12 @@
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
 public class CourierData {
     private String login;
     private String password;
     private String firstName;
-
-    // конструктор со всеми параметрами
-    public CourierData(String login, String password, String firstName) {
-        this.login = login;
-        this.password = password;
-        this.firstName = firstName;
-    }
 
     public static CourierData withLoginAndPassword(String login, String password) {
         CourierData data = new CourierData();
@@ -32,27 +30,5 @@ public class CourierData {
 
     // конструктор без параметров
     public CourierData() {
-    }
-
-
-    public String getLogin() {
-        return login;
-    }
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 }
